989532e9e5c1ed1872274785564d87f5c3c0136b
sebhall <sebhalldesigns@gmail.com>
2023-12-17 20:04:22 +0000

    intial geomod classes & opengl demo

---
+++ Include/GeoMod.h
@@ -0,0 +1,66 @@
+//  geomod, a geometric modelling kernel by seb hall
+//  created on 21/06/2023
+//
+//  --- Features ---
+//
+//  geomod is a hybrid geometric kernel, designed for 
+//  both solid-style modelling, (e.g mechanical CAD) as 
+//  well as mesh-style modelling (e.g animation, game 
+//  design). It also features a heirarchical layout
+//  system, as well as advanced model loading and rendering. 
+//  Intended applications include mechanical and 
+//  architectural CAD software, animation design and
+//  surface modelling applications. 
+//
+
+#ifndef GEOMOD_H
+#define GEOMOD_H
+
+//  a point is a simple location in 3D space, with no
+//  regard to size, location, direction etc
+struct gPoint {
+    double x;
+    double y;
+    double z;
+};
+
+//  a line defines a 1-dimensional region between two points
+struct gLine {
+    gPoint start;
+    gPoint end;
+};
+
+
+//  a plane can be defined as the perpendicular bisector of a line
+// or simply as perpendicular to it's normal, sharing the 'start'
+struct gPlane {
+    gLine normal;
+};
+
+//  a circle can be defined as the perpendicular bisector of a line, 
+//  with constant radius or as a plane with a normal and a constant radius.
+struct gCircle {
+    gLine normal;
+    double radius;
+};
+
+
+// some other ideas
+
+
+// a generic ellipse can be defined by a centre point and two radial points.
+// these probably should be perpendicular but I'm not sure
+struct gEllipse {
+    gPoint central;
+    gPoint radialA;
+    gPoint radialB;
+};
+
+//  a triangle is used in mesh modelling
+struct gTriangle {
+    gPoint a;
+    gPoint b;
+    gPoint c;
+};
+
+#endif // GEOMOD_H
---
+++ main.cpp
@@ -0,0 +1,233 @@
+
+#include <cstdio>
+
+#ifndef UNICODE
+#define UNICODE
+#endif 
+
+#include <Windows.h>
+#include <windowsx.h>
+#include <gl/GL.h>
+#include <gl/GLU.h>
+#include <vector>
+
+#include "Include/GeoMod.h"
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+HGLRC glRenderContextHandle;
+float width;
+float height;
+int currentMouseXPos;
+int currentMouseYPos;
+
+float originX = 0.0f;
+float originY = 0.0f;
+
+float scale = 1.0f;
+
+std::vector<gPoint> points;
+
+int main() {
+
+    printf("hello world\n");
+
+    const wchar_t CLASS_NAME[]  = L"Sample Window Class";
+    
+    WNDCLASS wc = { };
+
+    wc.lpfnWndProc   = WindowProc;
+    wc.hInstance     = GetModuleHandle(nullptr);
+    wc.lpszClassName = CLASS_NAME;
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+    RegisterClass(&wc);
+    // Create the window.
+
+    HWND hwnd = CreateWindowEx(
+        0,                              // Optional window styles.
+        CLASS_NAME,                     // Window class
+        L"Learn to Program Windows",    // Window text
+        WS_OVERLAPPEDWINDOW,            // Window style
+
+        // Size and position
+        800, 600, CW_USEDEFAULT, CW_USEDEFAULT,
+
+        NULL,       // Parent window    
+        NULL,       // Menu
+        GetModuleHandle(nullptr),  // Instance handle
+        NULL        // Additional application data
+        );
+
+    if (hwnd == NULL)
+    {
+        return 0;
+    }
+
+    PIXELFORMATDESCRIPTOR pfd = {
+        sizeof(PIXELFORMATDESCRIPTOR),
+        1,
+        PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+        PFD_TYPE_RGBA,
+        24, // bit depth
+        0, 0, 0, 0, 0, 0,
+        0,
+        0,
+        0,
+        0, 0, 0, 0,
+        16, // depth buffer
+        0, // stencil buffer
+        0,
+        PFD_MAIN_PLANE,
+        0,
+        0, 0, 0
+    };
+
+    HDC hdc = GetDC(hwnd);
+    int pixelFormat = ChoosePixelFormat(hdc, &pfd);
+    SetPixelFormat(hdc, pixelFormat, &pfd);
+
+    glRenderContextHandle = wglCreateContext(hdc);
+    
+    wglMakeCurrent(hdc, glRenderContextHandle);
+    glViewport(0, 0, 800, 600);
+    
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+
+    glEnable(GL_BLEND);  
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);  
+    glEnable(GL_TEXTURE_2D);   
+    glEnable(GL_POINT_SMOOTH); 
+    glPixelStorei(GL_UNPACK_ALIGNMENT, 1); // disable byte-alignment restriction for text
+
+    ReleaseDC(hwnd, hdc);
+
+    ShowWindow(hwnd, SW_SHOW);
+
+    MSG msg = { };
+    while (GetMessage(&msg, NULL, 0, 0) > 0)
+    {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
+{
+    switch (uMsg)
+    {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_SIZE: {
+        width = (float)LOWORD(lParam);
+        height = (float)HIWORD(lParam);
+        InvalidateRect(hwnd, NULL, NULL);
+        return 0;
+    }
+
+    case WM_PAINT:
+        {
+
+            //printf("WM PAINT %f %f\n", width, height);
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hwnd, &ps);
+
+            // All painting occurs here, between BeginPaint and EndPaint.
+
+            wglMakeCurrent(hdc, glRenderContextHandle);
+            glViewport(0, 0, (int)width, (int)height);
+
+            glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+            glClear(GL_COLOR_BUFFER_BIT);
+
+            // rendering happens here
+            
+            glMatrixMode(GL_PROJECTION); // Switch to projection matrix mode
+            glLoadIdentity(); // Reset the projection matrix
+            
+            // Set up a perspective projection matrix
+            // Parameters: field of view, aspect ratio, near plane, far plane
+            //gluPerspective(45.0, width / height, 0.1, 100.0);
+            gluOrtho2D(0, width, height, 0); // Set these values to define your 2D plane
+
+            glMatrixMode(GL_MODELVIEW); // Switch back to modelview matrix mode
+
+            float gridScale = 100.0f * scale;
+
+            glColor4f(1.0f, 1.0f, 1.0f, 0.25f);
+            // Horizontal lines
+            for(float i = originY; i <= height; i += gridScale) {
+                glBegin(GL_LINES);
+                    glVertex2f(0.0f, i);
+                    glVertex2f(width, i);
+                glEnd();
+            }
+
+            // Vertical lines
+            for(float i = originX; i <= width; i += gridScale) {
+                glBegin(GL_LINES);
+                    glVertex2f(i, 0.0f);
+                    glVertex2f(i, height);
+                glEnd();
+            }
+            // end of rendering
+
+            glColor4f(1.0f, 1.0f, 1.0f, 0.5f);
+            glPointSize(7.5f);
+            glBegin(GL_POINTS);
+                glVertex2f((float)currentMouseXPos, (float)currentMouseYPos);
+                for (gPoint point : points) {
+                    glVertex2d((point.x + originX) * scale, (point.y + originY) * scale);
+                }
+                
+            glEnd();
+
+
+            SwapBuffers(hdc);
+            EndPaint(hwnd, &ps);
+        }
+        return 0;
+     case WM_MOUSEMOVE: {
+            int xPos = GET_X_LPARAM(lParam);
+            int yPos = GET_Y_LPARAM(lParam);
+
+            int dX = xPos - currentMouseXPos;
+            int dY = yPos - currentMouseYPos;
+
+            currentMouseXPos = xPos;
+            currentMouseYPos = yPos;
+
+            if (GetAsyncKeyState(VK_MBUTTON)) {
+                
+                originX += (float) dX;
+                originY += (float) dY;
+                
+            }
+            InvalidateRect(hwnd, NULL, NULL);
+            //printf("MOUSE POS %f %f\n", (float)currentMouseXPos, (float)currentMouseYPos);
+        }
+        return 0;
+    case WM_MOUSEWHEEL: {
+            int zDelta = GET_WHEEL_DELTA_WPARAM(wParam);
+            
+            scale *= (1.0f + (float)zDelta / 1000.0f);
+            printf("MOUSE SCROLL %f\n", scale);
+            InvalidateRect(hwnd, NULL, NULL);
+        }
+        return 0;
+    case WM_LBUTTONDOWN:
+            {
+                currentMouseXPos = GET_X_LPARAM(lParam);
+                currentMouseYPos = GET_Y_LPARAM(lParam);
+
+                points.push_back({((float) currentMouseXPos / scale) - originX, ((float) currentMouseYPos / scale) - originY});
+            }
+            return 0;
+    }
+    return DefWindowProc(hwnd, uMsg, wParam, lParam);
+}
