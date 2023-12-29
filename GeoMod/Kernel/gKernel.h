ffb1bb8e48f82ca8745832ef7076a750e4aa1286
sebhall <sebhalldesigns@gmail.com>
2023-12-29 17:24:01 +0000

    barebones kernel interface!!!

--- GeoMod/Kernel/gKernel.h
+++ GeoMod/Kernel/gKernel.h
@@ -2,12 +2,17 @@
 #define GKERNEL_H
 
 #include "gKernelServer.h"
+#include "gKernelFunction.h"
 #include "../Debug/gLog.h"
 
 #include <cstdint>
 #include <chrono>
 #include <csignal>
 #include <atomic>
+#include <vector>
+#include <functional>
+#include <string>
+
 
 // Global flag used to exit the program loop
 static std::atomic<bool> exitFlag(false);
@@ -26,11 +31,19 @@ class gKernel {
     }
 
 public:
+    static std::vector<gKernelFunction> startupModuleFuctions;
+
     static int KernelMain(int argc, char *argv[]) {
         // Set the signal handler for SIGINT
         std::signal(SIGINT, signalHandler);
-
+        
         uint64_t elapsedTime = 0;
+
+        for (gKernelFunction function : startupModuleFuctions) {
+            // init module passing an api handler as the 1 parameter
+            gKernelGenericParameter genericParameter =  { "gKernelFunction", gKernelFunction_t, &APIHandler };
+            function("InitModule", InitModule_f, 1, genericParameter);
+        }
 
         if (!gKernelServer::Init("C:\\GeoMod\\server.socket", 2023)) {
             return -1;
@@ -50,7 +63,36 @@ public:
         return 0;
     };
 
+    static void APIHandler(const char* functionRaw, size_t functionHash, size_t numParameters, ...) { 
+        switch (functionHash) {
+        case gLog_Info_f: {
+            va_list args;
+            va_start(args, numParameters);
+
+            for (int i = 0; i < numParameters; ++i) {
+                gKernelGenericParameter genericParameter = va_arg(args, gKernelGenericParameter);
+                switch (genericParameter.typeHashed) {
+                    case std_string_t: {
+                        gLog::Info("%s", static_cast<std::string*>(genericParameter.ptr)->c_str());
+                        
+                        break;
+                    }
+
+                    default: {
+                        gLog::Error("Escaping API call due to unknown parameter type %s", genericParameter.typeRaw);
+                        return;
+                    }
+                }
+            }
+
+            va_end(args);
+            return;
+        }
+    }
+    }
+
 };
 
+std::vector<gKernelFunction> gKernel::startupModuleFuctions;
 
 #endif // GKERNEL_H
--- main.cpp
+++ main.cpp
@@ -1,9 +1,49 @@
 #include "GeoMod/Debug/gLog.h"
 #include "GeoMod/Kernel/gKernel.h"
+#include "GeoMod/Kernel/gKernelFunction.h"
+#include <stdio.h>
+
+gKernelFunction kernelAPI;
+
+void MyAPIHandler(const char* functionRaw, size_t functionHash, size_t numParameters, ...) {
+    switch (functionHash) {
+        case InitModule_f: {
+            va_list args;
+            va_start(args, numParameters);
+
+            for (int i = 0; i < numParameters; ++i) {
+                gKernelGenericParameter genericParameter = va_arg(args, gKernelGenericParameter);
+                switch (genericParameter.typeHashed) {
+                    case gKernelFunction_t: {
+                        kernelAPI = static_cast<gKernelFunction>(genericParameter.ptr);
+                        gKernelGenericParameter genericParameter;
+                        std::string* myString = new std::string("hello kernel interface!");
+                        genericParameter.typeHashed = std_string_t;
+                        genericParameter.typeRaw = "std::string";
+                        genericParameter.ptr = myString;
+                        kernelAPI("gLog::Info", gLog_Info_f, 1, genericParameter);
+                        delete myString;
+                        break;
+                    }
+
+                    default: {
+                        gLog::Error("Escaping API call due to unknown parameter type %s", genericParameter.typeRaw);
+                        return;
+                    }
+                }
+            }
+
+            va_end(args);
+            return;
+        }
+    }
+}
 
 int main(int argc, char *argv[]) {
 
     gLog::Info("Starting GeoMod...");
+
+    gKernel::startupModuleFuctions.push_back(&MyAPIHandler);
 
     return gKernel::KernelMain(argc, argv);
 }
